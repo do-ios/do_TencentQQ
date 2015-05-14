08b8f8bdcebc3fefd12fa6f1857ad6ae9828b555
yangzhen <1784054000@qq.com>
2015-05-14 16:06:17 +0800

    重新上传资源

---
+++ ext_implement/do_TencentQQ_SM.h
@@ -0,0 +1,14 @@
+//
+//  do_TencentQQ_SM.h
+//  DoExt_SM
+//
+//  Created by @userName on @time.
+//  Copyright (c) 2015年 DoExt. All rights reserved.
+//
+
+#import "do_TencentQQ_ISM.h"
+#import "doSingletonModule.h"
+
+@interface do_TencentQQ_SM : doSingletonModule<do_TencentQQ_ISM>
+
+@end
